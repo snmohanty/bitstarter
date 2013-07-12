ece0f68fc9337d5ceb2896955de18fa079fffa76
snmohanty <smohanty22152@yahoo.com>
2013-07-12 10:35:33 -0400

    added web.js

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ var app = express.createServer(express.logger());
 buffer.write('hello hello');
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send('hello hello');
 });
 
 var port = process.env.PORT || 5000;
