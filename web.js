7390f465c3eeb9e24fb3298f6047d6595526301b
snmohanty <smohanty22152@yahoo.com>
2013-07-12 10:07:55 -0400

    added web.js

--- web.js
+++ web.js
@@ -1,22 +1,13 @@
 var express = require('express');
-var fs = require('fs');
-var buffer new buffer(256);
-
 
 var app = express.createServer(express.logger());
 
-fs.readFile('index.html', 'utf-8', function(err, data){
-    if (err) throw err;
-    return(data);
-})
-
-buffer.write("hello hello");
-
 app.get('/', function(request, response) {
-  response.send('hello hello');
+  response.send('hello world 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
