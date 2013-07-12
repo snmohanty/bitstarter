fe977dd20c77feaa5c1305c230826e729657ed91
snmohanty <smohanty22152@yahoo.com>
2013-07-12 08:50:24 -0400

    added updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var fs = require('fs');
+var buffer = new buffer(256);
 
 var app = express.createServer(express.logger());
 
 fs.readFile('index.html', function(err, data){
     if (err) throw err;
-    console.log(data);
+    buffer = data;
 })
 
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
